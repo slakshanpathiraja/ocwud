33696e158cfbd1894b93430a136b035ce4b5320d
slakshanpathiraja <59391037+slakshanpathiraja@users.noreply.github.com>
2020-08-06 13:49:51 +0530

    as

---
+++ Store/includes/footer.php
@@ -0,0 +1,142 @@
+<div class="footer-top-area-2">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-about-us"><center>
+                        <i class="fa fa-university" style='font-size:48px';></i>
+                        <h6 class="footer-wid-title-2">OUR PRODUCTS</h6>
+                        <p>Lorem ipsum dolor blanditiis iusto consequatur, modi aliquid eveniet eligendi iure eaque </center></p>
+                    </div>
+                </div>
+                
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-about-us"><center>
+                    <i class="fa fa-truck" style='font-size:48px';></i>
+                        <h6 class="footer-wid-title-2">OUR PRODUCTS</h6>
+                        <p>Lorem ipsum dolor blanditiis iusto consequatur, modi aliquid eveniet eligendi iure eaque </center></p>
+                    </div>
+                </div>
+                
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-about-us"><center>
+                    <i class="fa fa-credit-card" style='font-size:48px';></i>
+                        <h6 class="footer-wid-title-2">OUR PRODUCTS</h6>
+                        <p>Lorem ipsum dolor blanditiis iusto consequatur, modi aliquid eveniet eligendi iure eaque </center></p>
+                    </div>
+                </div>
+
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-about-us"><center>
+                    <i class="fa fa-life-ring" style='font-size:48px';></i>
+                        <h6 class="footer-wid-title-2">OUR PRODUCTS</h6>
+                        <p>Lorem ipsum dolor blanditiis iusto consequatur, modi aliquid eveniet eligendi iure eaque </center></p>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div> <!-- END SHIPPING BANNER -->
+
+<div class="footer-top-area">
+        <div class="zigzag-bottom"></div>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-about-us">
+                        <a href="./"><img src="img/ocwudw.png"width = 200px></a>
+                        <p>Lorem ipsum dolor blanditiis iusto consequatur, modi aliquid eveniet eligendi iure eaque ipsam iste, pariatur omnis sint! Suscipit, debitis, quisquam. Laborum commodi veritatis magni at?</p>
+                        <div class="footer-social">
+                            <a href="#" target="_blank"><i class="fa fa-facebook"></i></a>
+                            <a href="#" target="_blank"><i class="fa fa-twitter"></i></a>
+                            <a href="#" target="_blank"><i class="fa fa-youtube"></i></a>
+                            <a href="#" target="_blank"><i class="fa fa-linkedin"></i></a>
+                        </div>
+                    </div>
+                </div>
+                
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-menu">
+                        <h2 class="footer-wid-title">USEFUL LINKS </h2>
+                        <ul>
+                            <li><a href="#">Privacy Policys</a></li>
+                            <li><a href="#">Return Policys</a></li>
+                            <li><a href="#">Terms & Conditions</a></li>
+                            <li><a href="#">Contact Us</a></li>
+                            <li><a href="#">FAQs</a></li>
+                        </ul>                        
+                    </div>
+                </div>
+                
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-menu">
+                        <h2 class="footer-wid-title">OUR PRODUCTS</h2>
+                        <ul>
+                            <li><a href="#">Mobile Phone</a></li>
+                            <li><a href="#">Home accesseries</a></li>
+                            <li><a href="#">LED TV</a></li>
+                            <li><a href="#">Computer</a></li>
+                            <li><a href="#">Gadets</a></li>
+                        </ul>                        
+                    </div>
+                </div>
+                
+                <div class="col-md-3 col-sm-6">
+                    <div class="footer-newsletter">
+                        <h2 class="footer-wid-title">Newsletter</h2>
+                        <p>Sign up to our newsletter and get exclusive deals you wont find anywhere else straight to your inbox!</p>
+                        <div class="newsletter-form">
+                            <form action="#">
+                                <input type="email" placeholder="Type your email">
+                                <input type="submit" value="Subscribe">
+                            </form>
+                        </div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+    </div> <!-- End footer top area -->
+    
+    <div class="footer-bottom-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="copyright">
+                        <p>&copy; 2020 OCWUD. All Rights Reserved. <a href="https://skypipit.com" target="_blank">SKYPIPIT Team</a></p>
+                    </div>
+                </div>
+                
+                <div class="col-md-4">
+                    <div class="footer-card-icon">
+                        <i class="fa fa-cc-discover"></i>
+                        <i class="fa fa-cc-mastercard"></i>
+                        <i class="fa fa-cc-paypal"></i>
+                        <i class="fa fa-cc-visa"></i>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+</div> <!-- End footer bottom area -->
+   
+   <!-- Latest jQuery form server -->
+   <script src="https://code.jquery.com/jquery.min.js"></script>
+   
+   <!-- Bootstrap JS form CDN -->
+   <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
+   
+   <!-- jQuery sticky menu -->
+   <script src="js/owl.carousel.min.js"></script>
+   <script src="js/jquery.sticky.js"></script>
+   
+   <!-- jQuery easing -->
+   <script src="js/jquery.easing.1.3.min.js"></script>
+   
+   <!-- Main Script -->
+   <script src="js/main.js"></script>
+   
+   <!-- Slider -->
+   <script type="text/javascript" src="js/bxslider.min.js"></script>
+   <script type="text/javascript" src="js/script.slider.js"></script>
+ </body>
+</html>
---
+++ index.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+   <head>
+      <title>Ocwud</title>
+      <meta http-equiv = "refresh" content = "0; url = store" />
+   </head>
+   <body>
+   </body>
+</html>
